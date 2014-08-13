00c1619f0890cf6f90c09f704889a03e1d82c9a6
AJ <info@adaptedpro.net>
2014-08-12 17:23:09 -0700

    Map-it, Quick - adding basic functionality

--- public/js/mapit/default_mapit.js
+++ public/js/mapit/default_mapit.js
@@ -1,41 +1,33 @@
 $(function() {
+	//Set variables
     var rendererOptions = { draggable: true }; 
     var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);;
     var directionsService = new google.maps.DirectionsService();
-    var locale = new google.maps.LatLng(41.850033, -87.6500523);
+    var locale = "";
     var start = end = '';
-    var map;
     
-    $("#mapit_form").submit(function(e) { calcRoute(); });    
     $('#btn_route').click(function(e) { calcRoute(); });
-    
-    //Map Setup
+    $("#mapit-form").submit(function(e) { 
+    	calcRoute();
+    	return false; 
+    });    
+
+    //Request users location
     function initialize() {
-    	var mapOptions = {
-    			zoom: 7,
-    			center: locale
-    		};
-    	
-    	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    	directionsDisplay.setMap(map);
-    	directionsDisplay.setPanel(document.getElementById('directionsPanel'));
-
-    	google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
-    		computeTotalDistance(directionsDisplay.getDirections());
-    	});
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        }
     }
-
+    
     //Calculate Route
     function calcRoute() {
        start = $('#start_location').val();
        end = $('#end_location').val();
-        
-    	var request = {
+       var request = {
     			origin: start,
     			destination: end,
     			travelMode: google.maps.TravelMode.DRIVING
-    		};
-    	
+    		};    	
     	directionsService.route(request, function(response, status) {
     		if (status == google.maps.DirectionsStatus.OK) {
     			directionsDisplay.setDirections(response);
@@ -51,8 +43,31 @@ $(function() {
     		total += myroute.legs[i].distance.value;
     	}
     	total = total / 1000.0;
-    	$('#total').html(total + ' km');
+    	var label = "<h4>Directions&nbsp;&nbsp;";
+    	$('#total').html(label+'<span class="glyphicon glyphicon-print"></span>&nbsp;&nbsp;<span class="glyphicon glyphicon-envelope"></span></h4><br/>' + total + ' km');
     }
     
+    function showPosition(position) {
+    	locale = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    	mapLocation();
+    }  
+    
+    //
+    function mapLocation() {
+    	var loc = (locale!="") ? locale : new google.maps.LatLng(41.850033, -87.6500523);
+    	var map;
+    	var mapOptions = {
+    			zoom: 7,
+    			center: loc
+    		};
+    	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    	directionsDisplay.setMap(map);
+    	directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+    	google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
+    		computeTotalDistance(directionsDisplay.getDirections());
+    	});    	
+    }    
+    
+    //When map ready
     google.maps.event.addDomListener(window, 'load', initialize);
 });
